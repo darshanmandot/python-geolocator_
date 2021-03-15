6252446f84df52133a3e74cb77ad99c58351c8a9
darshanmandot <80697677+darshanmandot@users.noreply.github.com>
2021-03-15 20:02:49 +0530

    Add files via upload

---
+++ geo.py
@@ -0,0 +1,16 @@
+
+from geopy.extra.rate_limiter import RateLimiter
+import pandas as pd
+from geopy.geocoders import ArcGIS
+
+
+df = pd.read_csv("C:\\Users\\darsh\\Downloads\\us_address_sample.csv")
+
+
+locator = ArcGIS(timeout=100)
+geocode = RateLimiter(locator.geocode, min_delay_seconds=1)
+df['location'] = df['full_address'].apply(geocode)
+df['point'] = df['location'].apply(lambda loc: tuple(loc.point) if loc else None)
+df[['latitude', 'longitude']] = pd.DataFrame(df['point'].tolist(), index=df.index)
+df = df.drop(['full_address', 'location','point'], axis=1)
+df.to_csv('file2.csv')
